a594fb5782a7750edea9185807105f0206c2f01e
Hai Nguyen <hainguyen871112@gmail.com>
2018-12-09 17:13:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function shout(string) {
   return string.toUpperCase()
 }
-shout('hello')
+shout('Hello')
 function whisper(string) {
   return string.toLowerCase()
 }
-whisper('HELLO')
+whisper('Hello')
 function logShout(string) {
   console.log(string.toUpperCase())
 }
-logShout('hello')
+logShout('Hello')
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-logWhisper('HELLO')
+logWhisper('Hello')
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
